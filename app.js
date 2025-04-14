b7f043d53ca8465eee43f11a54e2f3314ce0f8b7
Roselene Aparecida da Silva Yokoyama <roselene_31@hotmail.com>
2025-04-14 20:23:51 -0300

    listas

--- app.js
+++ app.js
@@ -14,12 +14,14 @@ livrosCategoria.push("aventura")
 livrosCategoria.push("infantil")
 console.dir(livrosCategoria)
 
-document.getElementById("conteudo").innerHTML = livrosCategoria[0] + " - "
-document.getElementById("conteudo").innerHTML += livrosCategoria[1] + " - "
-document.getElementById("conteudo").innerHTML += livrosCategoria[2] + " - "
-document.getElementById("conteudo").innerHTML += livrosCategoria[3] + " - "
-document.getElementById("conteudo").innerHTML += livrosCategoria[4] + " - "
-document.getElementById("conteudo").innerHTML += livrosCategoria[5] 
+document.getElementById("conteudo").innerHTML = "<ul>"
+document.getElementById("conteudo").innerHTML += "<li>" + livrosCategoria[0] + "</li>"
+document.getElementById("conteudo").innerHTML += "<li>" + livrosCategoria[1] + "</li>"
+document.getElementById("conteudo").innerHTML += "<li>" + livrosCategoria[2] + "</li>"
+document.getElementById("conteudo").innerHTML += "<li>" + livrosCategoria[3] + "</li>"
+document.getElementById("conteudo").innerHTML += "<li>" + livrosCategoria[4] + "</li>"
+document.getElementById("conteudo").innerHTML += "<li>" + livrosCategoria[5] + "</li>"
+document.getElementById("conteudo").innerHTML += "</ul>" 
 
 
 
