9e8ba1ce912ac3008631fed4eec1fc8a967789f9
Roselene Aparecida da Silva Yokoyama <roselene_31@hotmail.com>
2025-04-14 20:11:30 -0300

    array

---
+++ app.js
@@ -0,0 +1,27 @@
+//console.dir(livros)
+//console.log(livros[2])
+//livros[3] = "religiosos"
+//console.dir(livros)
+//livros.push("fantasia")
+//console.dir(livros)
+
+let livrosCategoria = new Array()
+livrosCategoria.push("aventura")
+livrosCategoria.push("romance")
+livrosCategoria.push("religioso")
+livrosCategoria.push("ação")
+livrosCategoria.push("aventura")
+livrosCategoria.push("infantil")
+console.dir(livrosCategoria)
+
+document.getElementById("conteudo").innerHTML = livrosCategoria[0] + " - "
+document.getElementById("conteudo").innerHTML += livrosCategoria[1] + " - "
+document.getElementById("conteudo").innerHTML += livrosCategoria[2] + " - "
+document.getElementById("conteudo").innerHTML += livrosCategoria[3] + " - "
+document.getElementById("conteudo").innerHTML += livrosCategoria[4] + " - "
+document.getElementById("conteudo").innerHTML += livrosCategoria[5] 
+
+
+
+
+
